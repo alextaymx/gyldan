4670d26f67526a1b979fe271b09b59eccc5c495a
Alex Tay <alex.tay@ideagenplc.com>
2025-01-04 21:19:13 +0800

    coming soon template

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  redirects: async () => {
+    return [
+      {
+        has: [
+          {
+            type: 'host',
+            value: 'gyldan.com.my', // Replace with the domain you want to match
+          },
+        ],
+        source: '/',
+        destination: '/coming-soon.html',
+        permanent: false, // Set true if the redirect is permanent (HTTP 301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
